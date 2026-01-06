a83a67ed2f6247d98943b880f73e62b691323dde
p20250093-afk <p20250093@hyderabad.bits-pilani.ac.in>
2026-01-06 03:10:25 +0000

    Deploying to gh-pages from @ p20250093-afk/nn@0c3d5e50a95559df2773d2d552fea37220ec1ae1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -466,14 +466,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6E%69%74%69%73%68%6B%75%6D%61%72.%67%75%70%74%61@%68%79%64%65%72%61%62%61%64.%62%69%74%73-%70%69%6C%61%6E%69.%61%63.%69%6E", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/nitish-kumar-gupta-b04863143", "_blank");
         },
       },{
         id: 'social-rss',
@@ -487,14 +487,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=ZNU3u0wAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
