f42dd88bdc2cea75b555baee5e41a230ea6a16f7
p20250093-afk <p20250093@hyderabad.bits-pilani.ac.in>
2026-01-09 09:54:02 +0000

    Deploying to gh-pages from @ nits1114/LAB@4ec599dc7670082d084079fe8091a856028297c5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/LAB/cv/";
           },
+        },{id: "nav-people",
+          title: "people",
+          description: "members of the lab",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/LAB/people/";
+          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
